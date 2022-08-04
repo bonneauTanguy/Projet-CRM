3e9634112bc1fecd66772f182bc5a0bb9540bd39
tanguy.bonneau1 <tanguy.bonneau@livecampus.tech>
2022-08-04 20:44:54 +0200

    page d'accueil

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>Crud en php</title>
+    <title>Projet CRM</title>
 
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/responsive.css" rel="stylesheet">
