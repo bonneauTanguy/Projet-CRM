ed69052357b1ba0323791eb334ab24fcd736ad87
tanguy.bonneau1 <tanguy.bonneau@livecampus.tech>
2022-08-04 10:49:32 +0200

    PDO

---
+++ database.php
@@ -0,0 +1,33 @@
+<?php
+
+class Database
+{
+    private static $dbName = 'projetCRM';
+    private static $dbHost = '192.168.10.10';
+    private static $dbUsername = 'homestead';
+    private static $dbUserPassword = 'secret';
+    private static $cont = null;
+
+    public function __construct()
+    {
+        die('Init function is not allowed');
+    }
+
+    public static function connect()
+    {
+        if (null == self::$cont) {
+            try {
+                self::$cont = new PDO("mysql:host=" . self::$dbHost . ";" . "dbname=" . self::$dbName, self::$dbUsername, self::$dbUserPassword);
+            } catch (PDOException $e) {
+                die($e->getMessage());
+            }
+        }
+        return self::$cont;
+    }
+
+    public static function disconnect()
+    {
+        self::$cont = null;
+    }
+}
+?>
