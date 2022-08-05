f50c360b13a6b0198801b7477c6bdf8063ac9568
tanguy.bonneau1 <tanguy.bonneau@livecampus.tech>
2022-08-05 10:41:59 +0200

    Correction des erreurs

--- edit.php
+++ edit.php
@@ -1,11 +1,19 @@
-<?php require('database.php');
-    $pdo=Database::connect();
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "SELECT * FROM user where id =?";
-    $q = $pdo->prepare($sql);
-    $q->execute(array($id));
-    $data = $q->fetch(PDO::FETCH_ASSOC);
-    Database::disconnect();
+<?php require('database.php'); //on appelle notre fichier de config
+    $id = null;
+    if (!empty($_GET['id'])) {
+        $id = $_REQUEST['id'];
+    }
+    if (null == $id) {
+        header("location:index.php");
+    } else { //on lance la connection et la requete
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT * FROM Contact where id =?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        $data = $q->fetch(PDO::FETCH_ASSOC);
+        Database::disconnect();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
