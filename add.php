2b43023d170667c9cf85ece32c6b861eb28211bc
tanguy.bonneau1 <tanguy.bonneau@livecampus.tech>
2022-08-05 11:12:46 +0200

    Correction des erreurs

--- add.php
+++ add.php
@@ -1,6 +1,5 @@
 <?php require 'database.php';
     if($_SERVER["REQUEST_METHOD"]== "POST" && !empty($_POST)) { //on initialise nos messages d'erreurs;
-
         $firstnameError = '';
         $lastnameError = '';
         $phone_numberError = '';
